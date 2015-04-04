2c329bead4424b525fca461cd94c71188107e801
Soraismus <tertius_lupus@yahoo.com>
2015-04-04 15:11:56 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'rails',        '4.2.0'
+gem 'sass-rails',   '~> 5.0'
+gem 'uglifier',     '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'cough-rails',  '0.0.0'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+end
---
+++ config/initializers/cough_syrup_assets.rb
@@ -0,0 +1,51 @@
+#require 'tilt'
+
+class CoughSyrupTiltTemplate < Tilt::Template
+  self.default_mime_type = 'application/javascript'
+
+  @@default_bare = false
+
+  def self.default_bare
+    @@default_bare
+  end
+
+  def self.default_bare=(value)
+    @@default_bare = value
+  end
+
+  # DEPRECATED
+  def self.default_no_wrap
+    @@default_bare
+  end
+
+  # DEPRECATED
+  def self.default_no_wrap=(value)
+    @@default_bare = value
+  end
+
+  def self.engine_initialized?
+    defined? ::CoughSyrup
+  end
+
+  def initialize_engine
+    require_template_library 'cough_syrup'
+  end
+
+  def prepare
+    if !options.key?(:bare) and !options.key?(:no_wrap)
+      options[:bare] = self.class.default_bare
+    end
+  end
+
+  def evaluate(scope, locals, &block)
+    @output ||= ::CoughSyrup.compile(data, options)
+  end
+
+  def allows_script?
+    false
+  end
+end
+
+Sprockets.register_engine('.cough', CoughSyrupTiltTemplate)
+
+#Tilt.register(CoughSyrupTiltTemplate, 'cough')
