49e987223d36d67672ce9a044baa5bd101ca9aec
Soraismus <tertius_lupus@yahoo.com>
2015-04-21 12:08:11 -0400

    Add helpers to provide some of the app's default initial state.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def app_state
+    { debug: debug }
+  end
+
+  def debug
+    {
+      should_debug: Rails.env.development?,
+      debug_params: debug_content(params)
+    }
+  end
+
+  def debug_content(object)
+    Marshal::dump(object)
+    object = ERB::Util.html_escape(object.to_yaml)
+                      .gsub("  ", "&nbsp; ")
+                      .html_safe
+  end
+
+  def server_config
+    { id: 'react-node', prerender: true }
+  end
 end
