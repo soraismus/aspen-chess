e03875fe44c6c19b1927b292291f709f8a37ba88
Soraismus <tertius_lupus@yahoo.com>
2015-04-20 22:13:44 -0400

    Rewriter the server_hook file in coughsyrup.

--- app/assets/javascripts/server_hook.js
+++
@@ -1,6 +0,0 @@
-var React    = require('react');
-var MainView = require('./components/main_view');
-
-module.exports = function (props) {
-  return React.renderToString(React.createElement(MainView, props));
-};
