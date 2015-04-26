4e3bc2e44ad3c38ac75ee2534bcaec64a3547b75
Soraismus <tertius_lupus@yahoo.com>
2015-04-26 15:20:55 -0400

    Modify static_pages#home action for xhr requests; also set @users.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+    @users = fellow_players
+    if request.xhr?
+      render json: {
+          users: filter_user_attributes(@users),
+          current_user: filter_user_attributes(current_user),
+          pagination_html: nil,
+          alert: flash
+        }
+    end
   end
+
+  private
+
+    def fellow_players(user = current_user)
+      User.except(user)
+    end
+
 end
