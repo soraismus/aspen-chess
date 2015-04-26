a69bcc3ffe0b3e73c30f69b5e8f1ccc864635a1b
Soraismus <tertius_lupus@yahoo.com>
2015-04-26 15:22:55 -0400

    Add session helpers.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,47 @@
+require 'utilities'
+
 module SessionsHelper
+
+  def current_user
+    unless @current_user.nil?
+      @current_user
+    else
+      session_token = cookies[:session_token]
+      session_digest = Utilities.encrypt(session_token)
+      self.current_user = User.find_by(session_digest: session_digest)
+    end
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_in(user)
+    reset_session_digest unless current_user.nil?
+
+    session_token = Utilities.new_session_token
+    session_digest = Utilities.encrypt(session_token)
+
+    cookies.permanent[:session_token] = session_token
+    user.update_attribute(:session_digest, session_digest)
+
+    self.current_user = user
+  end
+
+  def log_out
+    reset_session_digest
+    cookies.delete(:session_token)
+    self.current_user = nil
+  end
+
+  private
+    def reset_session_digest
+      new_session_digest = Utilities.new_session_digest
+      current_user.update_attributes(session_digest: new_session_digest)
+    end
+
 end
