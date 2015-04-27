a33790be2e00fb566b6b942172601f0fdcbda0ae
Soraismus <tertius_lupus@yahoo.com>
2015-04-27 11:52:13 -0400

    Change names of routes that correspond to forms.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get    'signup' => 'users#new'
-  get    'login'  => 'sessions#new'
-  post   'login'  => 'sessions#new'
+  get    'signup' => 'users#signup_form'
+  get    'login'  => 'sessions#login_form'
+  get    'logout' => 'sessions#logout_form'
+
+  post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :users,    except: :new
+  resources :users, except: :new
 end
