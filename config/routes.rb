4f478fa1403e310961df35e7f4756955d0641831
Soraismus <tertius_lupus@yahoo.com>
2015-04-26 23:10:41 -0400

    Modify sessions-related routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'signup' => 'users#new'
-  get 'login'  => 'sessions#new'
+  get    'signup' => 'users#new'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users,    except: :new
-  resources :sessions, except: :new
 end
